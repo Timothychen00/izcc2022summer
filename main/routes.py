9ec01557e207d07e5619958de70dc22ce4337c64
Timothychen00 <52665482+Timothychen00@users.noreply.github.com>
2022-06-14 07:59:59 +0800

    基本框架

--- main/routes.py
+++ main/routes.py
@@ -5,3 +5,19 @@ app_route=Blueprint('izcc',__name__,static_folder='static',template_folder='../t
 def home():
     print(1)
     return render_template('index.html')
+
+@app_route.route('/aboutus')
+def about():
+    return render_template('aboutus.html')
+
+@app_route.route('classes')
+def classes():
+    return render_template('classes.html')
+
+@app_route.route('contact')
+def classes():
+    return render_template('contact.html')
+
+@app_route.route('/join')
+def join():
+    return render_template('join.html')
