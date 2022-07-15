23c7302d4fd6deb4adfc36efce13e20c3070bcf9
Timothychen00 <52665482+Timothychen00@users.noreply.github.com>
2022-07-15 21:47:29 +0800

    報錯處理
    1.報錯傳送錯誤訊息到linenotify
    2.報名成功傳送到linenotify
    3.修改環境變數
    4.requirements

--- main/routes.py
+++ main/routes.py
@@ -1,10 +1,15 @@
-import os,pymongo
+import os,pymongo,traceback,requests
 from flask import Blueprint, redirect, render_template,request,session,flash
 from dotenv import load_dotenv
 from main.model import db_model
 from main.forms import JoinForm
 load_dotenv()
 
+
+headers = {
+    "Authorization": "Bearer " + os.environ['TOKEN'],
+    "Content-Type": "application/x-www-form-urlencoded"
+}
 app_route=Blueprint('izcc',__name__,static_folder='static',template_folder='../templates')
 
 @app_route.route('/')
@@ -13,18 +18,22 @@ def home():
 
 @app_route.route('/join',methods=['POST',"GET"])
 def about():
-    form=JoinForm()
-    if form.validate_on_submit():
-        if not form.file.data:
-            form.file.errors.append('請上傳檔案')
+    try:
+        form=JoinForm()
+        if form.validate_on_submit():
+            if not form.file.data:
+                form.file.errors.append('請上傳檔案')
+                return render_template('join.html',form=form)
+            # print(form.name.name)
+            db_model.insert_data(form)
+            flash('上傳成功')
+            requests.post("https://notify-api.line.me/api/notify",headers=headers, params= {"message":"報名成功："+form.name.data})
             return render_template('join.html',form=form)
-        # print(form.name.name)
-        db_model.insert_data(form)
-        flash('上傳成功')
+            # for i in form:
+            #     print(i.label,i.data)
+        else:
+            for i in form:
+                print(i.name,i.errors)
         return render_template('join.html',form=form)
-        # for i in form:
-        #     print(i.label,i.data)
-    else:
-        for i in form:
-            print(i.name,i.errors)
-    return render_template('join.html',form=form)
+    except Exception:
+        requests.post("https://notify-api.line.me/api/notify",headers=headers, params= {"message":traceback.format_exc()})
