2074b5ca7f13ee1c54d1b9446c7bb8b17376defd
Timothychen00 <52665482+Timothychen00@users.noreply.github.com>
2022-07-18 18:11:59 +0800

    景美聯絡資訊+linebotdebug

--- main/routes.py
+++ main/routes.py
@@ -22,32 +22,32 @@ def page_not_found(e):
 
 @app_route.route('/join',methods=['POST',"GET"])
 def about():
-    # try:
-    form=JoinForm()
-    if form.validate_on_submit():
-        if not form.file.data:
-            form.file.errors.append('請上傳檔案')
+    try:
+        form=JoinForm()
+        if form.validate_on_submit():
+            if not form.file.data:
+                form.file.errors.append('請上傳檔案')
+                return render_template('join.html',form=form)
+
+            # print(form.name.name)
+            msg=db_model.insert_data(form)
+
+            if msg:
+                if 'file' in msg:
+                    if not msg['file']:
+                        form.file.errors.append(msg['file'])
+
+            if msg:
+                flash(str(list(msg.values())).replace('[','').replace(']','').replace('\'',''))
+            else:
+                flash('上傳成功')
+                requests.post("https://notify-api.line.me/api/notify",headers=headers, params= {"message":"報名成功："+form.name.data})
             return render_template('join.html',form=form)
-
-        # print(form.name.name)
-        msg=db_model.insert_data(form)
-
-        if msg:
-            if 'file' in msg:
-                if not msg['file']:
-                    form.file.errors.append(msg['file'])
-
-        if msg:
-            flash(str(list(msg.values())).replace('[','').replace(']','').replace('\'',''))
+            # for i in form:
+            #     print(i.label,i.data)
         else:
-            flash('上傳成功')
-            requests.post("https://notify-api.line.me/api/notify",headers=headers, params= {"message":"報名成功："+form.name.data})
+            for i in form:
+                print(i.name,i.errors)
         return render_template('join.html',form=form)
-        # for i in form:
-        #     print(i.label,i.data)
-    else:
-        for i in form:
-            print(i.name,i.errors)
-    return render_template('join.html',form=form)
-    # except Exception:
-    #     requests.post("https://notify-api.line.me/api/notify",headers=headers, params= {"message":traceback.format_exc()})
+    except Exception:
+        requests.post("https://notify-api.line.me/api/notify",headers=headers, params= {"message":traceback.format_exc()})
